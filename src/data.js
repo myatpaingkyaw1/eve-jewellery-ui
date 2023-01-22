dd45f7d77aa653a157321a0bbaf20f7bd451c613
myatpaingkyaw1 <myatpaingkyaw@gmail.com>
2023-01-22 21:59:06 +0630

    add photos and slide shows

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-photo/charming-model-with-dark-hair-shows-rich-golden-earrings-necklace-ring_8353-5040.jpg?w=740&t=st=1674397336~exp=1674397936~hmac=7478eebc0d2535accd248a2fdafc6e1acc024a69c5fb7a6221d569077d240091",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+
+    {
+        id: 2,
+        img: "https://img.freepik.com/free-photo/charming-model-with-dark-hair-shows-rich-golden-earrings-necklace-ring_8353-5040.jpg?w=740&t=st=1674397336~exp=1674397936~hmac=7478eebc0d2535accd248a2fdafc6e1acc024a69c5fb7a6221d569077d240091",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+
+    {
+        id: 3,
+        img: "https://img.freepik.com/free-photo/charming-model-with-dark-hair-shows-rich-golden-earrings-necklace-ring_8353-5040.jpg?w=740&t=st=1674397336~exp=1674397936~hmac=7478eebc0d2535accd248a2fdafc6e1acc024a69c5fb7a6221d569077d240091",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
